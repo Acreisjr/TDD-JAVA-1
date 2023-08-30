f22c74afde01a554c0fb04302362db2dea5a8ea6
unknown <acreisjunio@gmail.com>
2023-08-30 16:32:52 -0300

    WIP: falta corrigir teste

---
+++ src/java/main/com/example/Calculator.java
@@ -0,0 +1,7 @@
+package java.main.com.example;
+
+public class Calculator {
+    public int sum(int a, int b) {
+        return a + b;
+    }
+}
--- test/java/com/example/CalculatorTest.java
+++ test/java/com/example/CalculatorTest.java
@@ -1,4 +1,7 @@
 import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.main.com.example.Calculator;
+
 import org.junit.jupiter.api.Test;
 
 public class CalculatorTest {
